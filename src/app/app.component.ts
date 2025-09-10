fd16df64c0b7403f56d95d8c679b2a9697dd917b
Ana Berto <anapaulab3rto@gmail.com>
2025-09-10 19:00:01 -0300

    feat: add form para inpu da configuração da memoria

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { FormsModule } from '@angular/forms';
 import { Component } from '@angular/core';
 import { InputAddress } from './feature/input-address/input-address.component';
 import { MemoryView } from './feature/memory-view/memory-view.component';
@@ -5,12 +6,16 @@ import { PageTable } from './feature/page-table/page-table.component';
 
 @Component({
   selector: 'app-root',
-  imports: [InputAddress, MemoryView, PageTable],
+  imports: [FormsModule, InputAddress, MemoryView, PageTable],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'address-translator';
+  addressBits = 8;
+  pageSizeBytes = 2;
+  framesCount = 10;
+  replacementPolicy = 'FIFO'
 
 
 }
